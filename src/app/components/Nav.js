415441018cb86825a1ea814a34e92480dac6b252
Shubham Kashyap <shubhamkashyap0709@gmail.com>
2023-07-22 17:47:25 +0530

    small fix

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -16,22 +16,22 @@ const Nav = () => {
                         <li className={styles.navbarItem}>
                             <Link className={styles.navbarLink} href="/"
                                   onClick={() => setOpenMenu(false)}
-                            >Home</Link>
+                            >Home </Link>
                         </li>
                         <li className={styles.navbarItem}>
                             <Link className={styles.navbarLink} href="/about"
                                   onClick={() => setOpenMenu(false)}
-                            >About</Link>
+                            >About </Link>
                         </li>
                         <li className={styles.navbarItem}>
                             <Link className={styles.navbarLink}
                                   onClick={() => setOpenMenu(false)}
-                                  href="/movie">Movie</Link>
+                                  href="/movie">Movie </Link>
                         </li>
                         <li className={styles.navbarItem}>
                             <Link className={styles.navbarLink}
                                   onClick={() => setOpenMenu(false)}
-                                  href="/contact">Contact</Link>
+                                  href="/contact">Contact </Link>
                         </li>
                     </ul>
 
